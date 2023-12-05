9f48944e81bc10fd9f7d91e1c2c63b99fcdf85ba
slonic32 <139376262+slonic32@users.noreply.github.com>
2023-12-05 21:41:06 +0000

    Deploying to gh-pages from @ slonic32/goit-js-hw-09@13dfbdd057c01ceb3a079dc315886a75b04df3f7 🚀

---
+++ 01-color-switcher.61d04a58.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(o,1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.61d04a58.js.map
---
+++ 01-color-switcher.9edc34f2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;function o(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(o,1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9edc34f2.js.map
